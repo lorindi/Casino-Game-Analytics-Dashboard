395f7721e4e46df4f3469c0d958b2497679d971a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-02-25 14:37:38 +0200

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,68 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const users = [
+  { id: "1", name: "Lora", age: 28, isMarried: false },
+  { id: "2", name: "Ivan", age: 38, isMarried: false },
+];
+
+const typeDefs = `
+    type Query {
+        getUsers: [User]
+        getUserById(id: ID!): User
+
+    }
+
+    type Mutation {
+        createUser(name: String!,  
+        age: Int!
+        isMarried: Boolean! ): User
+    }
+
+    type User {
+        id: ID,
+        name: String
+        age: Int
+        isMarried: Boolean
+    }
+`;
+
+const resolvers = {
+  Query: {
+    getUsers: () => {
+      return users;
+    },
+    getUserById: (parent, args) => {
+      const id = args.id;
+      return users.find((user) => user.id === id);
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const { name, age, isMarried } = args;
+      const newUser = {
+        id: (users.length + 1).toString(),
+        name,
+        age,
+        isMarried,
+      };
+      console.log(newUser);
+      users.push(newUser);
+      return newUser;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server running at: ${url}`);
+
+// Query, Mutation
+// typeDefs, resolvers
